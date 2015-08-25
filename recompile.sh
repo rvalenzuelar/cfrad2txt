80da09963d843db31b32ce98e3abd2a4e605df93
rvalenzuelar <raul.valenzuela@colorado.edu>
2015-08-25 15:55:40 -0600

    removed unnecesary lines

--- recompile.sh
+++ recompile.sh
@@ -10,12 +10,4 @@ FLIB="-L$NETCDF/lib -lnetcdf -lnetcdff -lcurl -lhdf5 -lhdf5_hl"
 
 FINC="-I$NETCDF/include -I/usr/include/geotiff"
 
-# for home
-# CLIB="-L/usr/lib -lgeotiff -ltiff -L/usr/local/lib"
-
-# for work
-CLIB="-lgeotiff -ltiff"
-
-CINC="-I/usr/include/geotiff"
-
 gfortran -g -o netcdf2text netcdf2text.f90 $FLIB $FINC
